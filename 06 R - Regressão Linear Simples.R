ffd8b99bc79ac663a37475691be3bfd03abbb4fe
Leonardo Neves Duarte <40400765+leohiei@users.noreply.github.com>
2019-07-25 14:57:49 -0300

    Update 06 R - Regressão Linear Simples.R

--- 06 R - Regressão Linear Simples.R
+++ 06 R - Regressão Linear Simples.R
@@ -35,5 +35,45 @@ modelo$coefficients[1] + modelo$coefficients[2] * 22
 #Obtendo o resultado com a função predict
 predict(modelo, data.frame(dist=22))
 
+-----------------------------------------
+> dim(cars)
+[1] 50  2
+> head(cars)
+  speed dist
+1     4    2
+2     4   10
+3     7    4
+4     7   22
+5     8   16
+6     9   10
+> cor(cars)
+          speed      dist
+speed 1.0000000 0.8068949
+dist  0.8068949 1.0000000
+> modelo = lm(speed ~ dist, data=cars)
+> modelo
 
+Call:
+lm(formula = speed ~ dist, data = cars)
 
+Coefficients:
+(Intercept)         dist  
+     8.2839       0.1656  
+
+> plot(modelo)
+Hit <Return> to see next plot: 
+Hit <Return> to see next plot: 
+Hit <Return> to see next plot: 
+Hit <Return> to see next plot: 
+> plot(speed ~ dist, data=cars)
+> abline(modelo)
+> modelo$coefficients
+(Intercept)        dist 
+  8.2839056   0.1655676 
+> modelo$coefficients[1] + modelo$coefficients[2] * 22
+(Intercept) 
+   11.92639 
+> predict(modelo, data.frame(dist=22))
+       1 
+11.92639 
+> 
