de2e50192d6964066a85d8f9fecc571301985ed4
Leonardo Neves Duarte <40400765+leohiei@users.noreply.github.com>
2019-07-31 17:58:52 -0300

    Update and rename 07 R - Regressão Linear Múltipla.R to 007 R - Regressão Linear Múltipla.R

--- 007 R - Regressão Linear Múltipla.R
+++ 007 R - Regressão Linear Múltipla.R
@@ -44,3 +44,51 @@ summary(modelo)$adj.r.squared
 #aqui podemos fazer a previsao da mesma forma, a diferenca eh que eh preciso atribuir
   #valores a todas as vars explanatorias que usamos no modelo
 predict(modelo, data.frame(disp=200, hp=100, cyl=4))
+----------------------------------------------------
+> colnames(mtcars)
+ [1] "mpg"  "cyl"  "disp" "hp"   "drat" "wt"   "qsec" "vs"   "am"   "gear" "carb"
+> dim(mtcars)
+[1] 32 11
+> cor(mtcars[1:4])
+            mpg        cyl       disp         hp
+mpg   1.0000000 -0.8521620 -0.8475514 -0.7761684
+cyl  -0.8521620  1.0000000  0.9020329  0.8324475
+disp -0.8475514  0.9020329  1.0000000  0.7909486
+hp   -0.7761684  0.8324475  0.7909486  1.0000000
+> modelo = lm(mpg ~ disp, data=mtcars)
+> modelo
+
+Call:
+lm(formula = mpg ~ disp, data = mtcars)
+
+Coefficients:
+(Intercept)         disp  
+   29.59985     -0.04122  
+
+> summary(modelo)$r.squared
+[1] 0.7183433
+> summary(modelo)$adj.r.squared
+[1] 0.7089548
+> plot(mpg ~ disp, data = mtcars)
+> abline(modelo)
+> predict(modelo, data.frame(disp=200))
+       1 
+21.35683 
+> modelo = lm(mpg ~ disp + hp + cyl, data = mtcars)
+> modelo
+
+Call:
+lm(formula = mpg ~ disp + hp + cyl, data = mtcars)
+
+Coefficients:
+(Intercept)         disp           hp          cyl  
+   34.18492     -0.01884     -0.01468     -1.22742  
+
+> summary(modelo)$r.squared
+[1] 0.7678877
+> summary(modelo)$adj.r.squared
+[1] 0.7430186
+> predict(modelo, data.frame(disp=200, hp=100, cyl=4))
+       1 
+24.03969 
+> 
