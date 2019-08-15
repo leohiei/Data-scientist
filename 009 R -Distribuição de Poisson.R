fb55365d067734f641390a85817b8bd12a0f810b
Leonardo Neves Duarte <40400765+leohiei@users.noreply.github.com>
2019-08-15 13:45:31 -0300

    Add files via upload

---
+++ 009 R -Distribuição de Poisson.R
@@ -0,0 +1,10 @@
+# o num de acidentes de carro por dia eh 2, qual a probabde em 1 dia ocorrer 3 acidentes?
+  # X = 3, media(lambda) = 2
+# calculo da probab
+dpois(3, lambda = 2)
+# o num de acidentes de carro por dia eh 2, qual a probab de em 1 dia ocorrer 3 ou menos acidentes?
+  # X <= 3, media(lambda) = 2
+ppois(3, lambda = 2)
+# o num de acidentes de carro por dia eh 2, qual a probabde em 1 dia ocorrer mais que 3 acidentes?
+  # X > 3, media(lambda) = 2
+ppois(3, lambda = 2, lower.tail = F)
