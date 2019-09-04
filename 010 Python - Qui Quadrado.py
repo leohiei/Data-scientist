bfaeaf598082266d22d634aae0baae4beaaf6277
Leonardo Neves Duarte <40400765+leohiei@users.noreply.github.com>
2019-09-04 16:53:41 -0300

    Update 010 Python - Qui Quadrado.py

--- 010 Python - Qui Quadrado.py
+++ 010 Python - Qui Quadrado.py
@@ -7,3 +7,13 @@ chi2_contingency(novela)
 # o parametro que importa eh o segundo, este eh o valor de P, por ser maior que
     # o alfa (0.05) nao podemos rejeitar a hipótese nula, nesse caso nao ha
         # diferenca significativa alem do acaso
+-------------
+import numpy as np
+
+from scipy.stats import chi2_contingency
+
+novela = np.array([[19, 6], [46, 32]])
+
+chi2_contingency(novela)
+(1.6825632201806293, 0.19458420874360915, 1, array([[15.77669903,  9.22330097],
+        [49.22330097, 28.77669903]]))
