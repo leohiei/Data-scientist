990e5a1fae531b67cb14bdda963aefe4a0781cf7
Leonardo Neves Duarte <40400765+leohiei@users.noreply.github.com>
2019-09-04 16:37:52 -0300

    Update 010 R - Qui Quadrado.R

--- 010 R - Qui Quadrado.R
+++ 010 R - Qui Quadrado.R
@@ -13,3 +13,15 @@ fix(novela)
 chisq.test(novela)
 # o resultado de p-value mostra que nao rejeitamos a hipotese nula (por ser inferio a 0.5), ou seja,
   # nao ha diferenca significa entre os valores alem do acaso
+-------------------
+> novela = matrix(c(19, 6, 43, 32), nrow=2, byrow=T)
+> fix(novela)
+> rownames(novela) = c('Masculino', 'Feminino')
+> colnames(novela) = c('Assiste', 'Nao assiste')
+> fix(novela)
+> chisq.test(novela)
+
+	Pearson's Chi-squared test with Yates' continuity correction
+
+data:  novela
+X-squared = 2.0374, df = 1, p-value = 0.1535
