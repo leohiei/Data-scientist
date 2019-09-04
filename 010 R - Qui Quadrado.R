cdd0a91db6cb86b3b538930f1533095247edf5b9
Leonardo Neves Duarte <40400765+leohiei@users.noreply.github.com>
2019-09-04 16:33:38 -0300

    Add files via upload

---
+++ 010 R - Qui Quadrado.R
@@ -0,0 +1,15 @@
+# teste de qui quadrado
+  #criando a funcao de tabela
+    #nrow(num de linhas), byrow=T distribuir valores por linha
+novela = matrix(c(19, 6, 43, 32), nrow=2, byrow=T)
+fix(novela)
+#nomeando linhas e colunas
+  #rownames (nome das linhas)
+  #colnames (nome das colunas)
+rownames(novela) = c('Masculino', 'Feminino')
+colnames(novela) = c('Assiste', 'Nao assiste')
+fix(novela)
+# chamando a funcao qui de quadrado
+chisq.test(novela)
+# o resultado de p-value mostra que nao rejeitamos a hipotese nula (por ser inferio a 0.5), ou seja,
+  # nao ha diferenca significa entre os valores alem do acaso
